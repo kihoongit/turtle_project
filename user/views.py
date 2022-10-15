bc17e799c57760f2e2e4d0cfb2132b5ce02ed26a
kihoongit <kitto5537@gmail.com>
2022-10-16 02:42:28 +0900

    회원가입 구현

--- user/views.py
+++ user/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
+from .models import User
 
 # Create your views here.
 def signup(request):
     if request.method == 'GET':
         return render(request, 'signup.html')
+
+    if request.method == 'POST':
+       username = request.POST.get('username')
+       password = request.POST.get('password')
+       passwordcheck = request.POST.get('passwordcheck')
+       if password == passwordcheck:
+        print(username,password)
+        User.objects.create_user(username=username, password=password)
+
+        return render(request, 'signup.html')
+
+
